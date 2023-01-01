05b02e5ef967359c0c941bbbfc128edfe05380ae
Hyun-s <hhs1516@gmail.com>
2023-01-01 16:59:53 +0900

    model downloader

---
+++ src/downloader/file_id.py
@@ -0,0 +1,10 @@
+e4e = {
+    "ffhq_encode": {"id": "1cUv_reLE6k3604or78EranS7XzuVMWeO", "name": "e4e_ffhq_encode.pt"},
+    "cars_encode": {"id": "17faPqBce2m1AQeLCLHUVXaDfxMRU2QcV", "name": "e4e_cars_encode.pt"},
+    "horse_encode": {"id": "1TkLLnuX86B_BMo2ocYD0kX9kWh53rUVX", "name": "e4e_horse_encode.pt"},
+    "church_encode": {"id": "1-L0ZdnQLwtdy6-A_Ccgq5uNJGTqE7qBa", "name": "e4e_church_encode.pt"}
+}
+
+style_gan{
+    "ffhq_f":{"id": "1EM87UquaoQmk17Q8d5kYIAHqu0dkYqdT". "name": "stylegan2-ffhq-config-f.pt"}
+}
