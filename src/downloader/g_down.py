66b27f9f0bfc4dc4ab1356637dfdfe0c4ff63657
Hyun-s <hhs1516@gmail.com>
2023-01-02 15:20:18 +0900

    update

--- src/downloader/g_down.py
+++ src/downloader/g_down.py
@@ -2,6 +2,7 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from google.colab import auth
 from oauth2client.client import GoogleCredentials
+import os
 import subprocess
 
 class g_down(object):
