389a27a9a943aef6b4dc5886cd3ef2f5becb9f50
Hyun-s <hhs1516@gmail.com>
2023-01-02 14:58:22 +0900

    update

--- src/downloader/g_down.py
+++ src/downloader/g_down.py
@@ -29,5 +29,5 @@ class g_down(object):
             downloaded.FetchMetadata(fetch_all=True)
             downloaded.GetContentFile(file_dst)
         else:
-            subprocess.run(!gdown --id $file_id -O $file_dst)
+            subprocess.run('!gdown --id '+file_id+ '-O '+file_dst')
             
