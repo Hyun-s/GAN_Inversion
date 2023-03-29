b78c9cdeaffedb1ab51fa272078f55823bac3e4b
Hyun-s <hhs1516@gmail.com>
2023-03-29 18:33:12 +0900

    use wandb

--- ood_tuning/runners/controllers/running_logger.py
+++ ood_tuning/runners/controllers/running_logger.py
@@ -94,6 +94,7 @@ class RunningLogger(BaseController):
 
         # Save in text format.
         msg = f'Iter {runner.iter:6d}/{runner.total_iters:6d}'
+        print(runner.running_stats)
         msg += f', {runner.running_stats}'
         memory = torch.cuda.max_memory_allocated() / (1024 ** 3)
         msg += f' (memory: {memory:.1f}G)'
